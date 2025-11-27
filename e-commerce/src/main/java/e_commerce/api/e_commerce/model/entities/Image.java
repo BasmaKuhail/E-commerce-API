0c1b83143172c7335e4dcab423a210baad690711
BasmaKuhail <basmakuhail2003@gmail.com>
2025-11-27 15:20:44 +0200

    Image Entity

---
+++ e-commerce/src/main/java/e_commerce/api/e_commerce/model/entities/Image.java
@@ -0,0 +1,81 @@
+package e_commerce.api.e_commerce.model.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String imageURL;
+
+    @Column(nullable = false)
+    private String  altText;
+
+    private Boolean isPrimary;
+    private int sortOrder;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    Product product;
+
+    public Image() {
+    }
+
+    public Image(Long id, String imageURL, String altText, Boolean isPrimary, int sortOrder, Product product) {
+        this.id = id;
+        this.imageURL = imageURL;
+        this.altText = altText;
+        this.isPrimary = isPrimary;
+        this.sortOrder = sortOrder;
+        this.product = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public Boolean getPrimary() {
+        return isPrimary;
+    }
+
+    public void setPrimary(Boolean primary) {
+        isPrimary = primary;
+    }
+
+    public String getAltText() {
+        return altText;
+    }
+
+    public void setAltText(String altText) {
+        this.altText = altText;
+    }
+
+    public int getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(int sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
--- e-commerce/src/main/java/e_commerce/api/e_commerce/model/entities/Product.java
+++ e-commerce/src/main/java/e_commerce/api/e_commerce/model/entities/Product.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 @Entity
 public class Product {
