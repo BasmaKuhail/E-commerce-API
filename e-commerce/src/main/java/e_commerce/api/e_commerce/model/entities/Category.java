b1ee202505e45011915ae588358dd7950ec84afd
BasmaKuhail <basmakuhail2003@gmail.com>
2025-11-27 15:03:01 +0200

    Category Entity

---
+++ e-commerce/src/main/java/e_commerce/api/e_commerce/model/entities/Category.java
@@ -0,0 +1,71 @@
+package e_commerce.api.e_commerce.model.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    private String logoURL;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Product> products = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Category(Long id, String description, String name, String logoURL) {
+        this.id = id;
+        this.description = description;
+        this.name = name;
+        logoURL = logoURL;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLogoURL() {
+        return logoURL;
+    }
+
+    public void setLogoURL(String logoURL) {
+        logoURL = logoURL;
+    }
+}
