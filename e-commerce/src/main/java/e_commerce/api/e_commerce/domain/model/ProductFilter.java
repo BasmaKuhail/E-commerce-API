24290f99036d2b205f5ff520ea8ca872822c8f00
BasmaKuhail <basmakuhail2003@gmail.com>
2026-01-03 14:44:23 +0200

    update finad all products functionalty to use pagination for data loading seprate search products by filtter for scalability

---
+++ e-commerce/src/main/java/e_commerce/api/e_commerce/domain/model/ProductFilter.java
@@ -0,0 +1,40 @@
+package e_commerce.api.e_commerce.domain.model;
+
+public class ProductFilter {
+    private String category;
+    private String brand;
+    private String tag;
+
+    public ProductFilter() {
+    }
+
+    public ProductFilter(String category, String brand, String tag) {
+        this.category = category;
+        this.brand = brand;
+        this.tag = tag;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
---
+++ e-commerce/src/main/java/e_commerce/api/e_commerce/domain/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package e_commerce.api.e_commerce.domain.repository;
+
+import e_commerce.api.e_commerce.domain.model.Product;
+import e_commerce.api.e_commerce.domain.model.ProductFilter;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    public List<Product> searchProducts(ProductFilter productFilter, Pageable pageable);
+}
